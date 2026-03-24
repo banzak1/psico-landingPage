c85ffa965237f48197ff5e976c9bdc040b3748af
Banzak <leosantanabr@gmail.com>
2026-03-24 08:26:31 -0300

    feat: implement contact form in footer with Firebase integration for lead submission

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService
+  ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'psico-landingpage',
+    appId: '1:457918869346:web:a20b1d0d40bccf96fd063b',
+    storageBucket: 'psico-landingpage.firebasestorage.app',
+    apiKey: 'AIzaSyACCOra3V0Sw35mA33wo_crv4ig--vgQE8',
+    authDomain: 'psico-landingpage.firebaseapp.com',
+    messagingSenderId: '457918869346',
+    measurementId: 'G-XTGFLCYKKD',
+  }
+};
