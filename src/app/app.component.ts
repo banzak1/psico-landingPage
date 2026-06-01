e8ed18fef98978410ad8338855768ae61adb2358
Banzak <leosantanabr@gmail.com>
2026-06-01 01:20:06 -0300

    refactor: replace direct landing components with router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { HeroComponent } from './features/landing/hero/hero.component';
-import { AboutComponent } from './features/landing/about/about.component';
-import { ServicesComponent } from './features/landing/services/services.component';
-import { HowItWorksComponent } from './features/landing/how-it-works/how-it-works.component';
-import { FaqComponent } from './features/landing/faq/faq.component';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +9,7 @@ import { FaqComponent } from './features/landing/faq/faq.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    FooterComponent,
-    HeroComponent,
-    AboutComponent,
-    ServicesComponent,
-    HowItWorksComponent,
-    FaqComponent
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
